d300c6b17fcac34edf1bc959c051dfa729df388e
dzikizgon669 <edyta.janczak99@gmail.com>
2022-02-13 23:26:50 +0100

    After Changes

---
+++ z4.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+
+
+int zgadnij (int podana)
+{
+
+int proby =2;
+
+int probyy =6;
+
+    int liczba, X=1, Y=20;
+    srand(time(NULL));
+    liczba=X + rand()%(Y-X+1);
+
+
+    for (int i = 1; i <= proby; i++){
+
+        if (podana!=liczba)
+        {
+         cout<< "Nie zgadles. Podaj ponownie liczbe: "<<endl;
+        cin>> podana;
+
+        }
+
+         else  {
+
+            cout <<"Zgadles!!!"<<endl;
+            break;
+            }
+    }
+
+    for (int i = 1; i <= probyy; i++){
+
+        if(liczba<podana){
+
+            cout<<"twoja strzal jest wiekszy od liczby"<<endl;
+            cout<<"wprowadz ponownie"<<endl;
+            cin>>podana;
+        }
+
+            else {
+
+
+            cout<<"twoja strzal jest mniejszy od liczby"<<endl;
+            cout<<"wprowadz ponownie"<<endl;
+            cin>>podana;
+
+            }
+
+           if (liczba==podana){
+
+                cout<<"wygrales"<<endl;
+                break;
+            }
+
+
+
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+int main()
+{
+    int podana;
+
+    cout <<"Musisz odgadnac liczbe calkowita z przedzialu od 1 do 20. Wybierz liczbe: "<<endl;
+    cin >>podana;
+
+
+
+    cout <<zgadnij (podana)<<endl;
+
+
+
+
+}
